14d6cff246a8056801db2c8c04f1183df2fa0ec1
shivasaicharanruthala <ruthala.s@northeastern.edu>
2023-10-25 18:09:46 -0400

    modifies user permissions

--- startup-scripts/service-startup.sh
+++ startup-scripts/service-startup.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
+
 sudo groupadd ec2-user
 sudo useradd -s /bin/false -g ec2-user ec2-user
+
 sudo cp ./startup-scripts/webapp.service /etc/systemd/system
+
+sudo chown -R ec2-user:ec2-user /home/webapp
+sudo chown -R ec2-user:ec2-user /etc/systemd/system/webapp.service
+sudo u+x /etc/systemd/system/webapp.service
+
 sudo systemctl daemon-reload
-sudo systemctl enable webapp
-sudo systemctl start webapp
-sudo systemctl restart webapp
+sudo -u ec2-user systemctl enable webapp
+sudo -u ec2-user systemctl start webapp
+sudo -u ec2-user systemctl restart webapp

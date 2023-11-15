88d9a393fb1343ce46792476148817d40bb8957d
shivasaicharanruthala <ruthala.s@northeastern.edu>
2023-11-15 18:39:21 -0500

    Modifies middleware

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -2,14 +2,29 @@ package middleware
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/shivasaicharanruthala/webapp/types"
 	"net/http"
+	"strings"
 )
 
 func APICountMetrics(ctx *types.Context) func(inner http.Handler) http.Handler {
 	return func(inner http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx.Metrics.Increment(fmt.Sprintf("api_calls.%s", r.Method+"-"+r.RequestURI))
+			var path string
+
+			if strings.Contains(r.RequestURI, "/v1/assignments") {
+				path = "/v1/assignments"
+
+				_, exists := mux.Vars(r)["id"]
+				if exists {
+					path += "/{id}"
+				}
+			} else {
+				path = r.RequestURI
+			}
+
+			ctx.Metrics.Increment(fmt.Sprintf("api_calls.%s", r.Method+"-"+path))
 
 			inner.ServeHTTP(w, r)
 		})
